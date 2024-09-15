1c8f5a7b57227550bc1f2c1a7534f32efd09abcf
Rodolfo Aguirre <aguirreg.rodolfo@gmail.com>
2024-09-15 05:25:35 +0000

    Copy fix from ascpixi/disconnect-packet-fix

---
+++ src/main/java/dev/roaguirr/disconnectPacketFix/mixin/ConnectionMixin.java
@@ -0,0 +1,42 @@
+/**
+ * Taken from: https://github.com/ascpixi/disconnect-packet-fix
+ */
+
+package dev.roaguirr.disconnectPacketFix.mixin;
+
+import dev.roaguirr.disconnectPacketFix.DisconnectPacketFixMod;
+
+import io.netty.buffer.ByteBuf;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.network.handler.PacketCodecDispatcher;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+@Mixin(PacketCodecDispatcher.class)
+public abstract class ConnectionMixin {
+    @Final
+    @Shadow
+    private Function<Object, ?> packetIdGetter;
+
+    @Final
+    @Shadow
+    private Object2IntMap<Object> typeToIndex;
+
+    @Inject(at = @At("HEAD"), method = "encode(Lio/netty/buffer/ByteBuf;Ljava/lang/Object;)V", cancellable = true)
+    private void encodeMixin(ByteBuf byteBuf, Object object, CallbackInfo info) {
+        var packetId = this.packetIdGetter.apply(object);
+        if (!this.typeToIndex.containsKey(packetId)) {
+            if (Objects.equals(String.valueOf(packetId), "clientbound/minecraft:disconnect")) {
+                DisconnectPacketFixMod.LOGGER.debug("Caught an invalid disconnect packet.");
+                info.cancel();
+            }
+        }
+    }
+}
